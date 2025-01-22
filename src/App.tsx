98257e922f5c59a8f7a7a17d2b6fe467f281e3ce
init-armat <armat@init.red>
2025-01-22 12:56:10 +0500

    feat: added tailwind, scss, cn function

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { cn } from "./utils/cn";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [counter, setCounter] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="flex flex-col items-center justify-center gap-3 h-screen w-full">
+      <h1
+        className={cn("text-5xl font-bold text-black", {
+          "text-red-500": counter > 0,
+          "text-blue-500": counter < 0,
+        })}
+      >
+        {counter}
+      </h1>
+      <div className="flex gap-1">
+        <button
+          onClick={() => setCounter((prev) => prev - 1)}
+          className="size-6 bg-gray-200 hover:bg-gray-300 transition-all"
+        >
+          -
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <button
+          onClick={() => setCounter((prev) => prev + 1)}
+          className="size-6 bg-gray-200 hover:bg-gray-300 transition-all"
+        >
+          +
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
